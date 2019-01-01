886478fead83e92bda136376a28c6d9671e8cc26
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-01 23:13:02 +0800

    修改io问题

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -39,16 +38,16 @@ func Identify(w http.ResponseWriter, r *http.Request) {
 	}
 	defer func() { _ = file.Close() }()
 
-	// save image for test
-	_, _ = SaveFace(file)
-
-	buf := bytes.NewBuffer(nil)
-	if _, err := io.Copy(buf, file); err != nil {
+	buf, err := ioutil.ReadAll(file)
+	if  err != nil {
 		_ = json.NewEncoder(w).Encode(Response{Status: "Data error\n"})
 		return
 	}
 
-	recoginzeFace, err := rec.RecognizeSingle(buf.Bytes())
+	// save image for test
+	_, _ = SaveFace(ioutil.NopCloser(bytes.NewBuffer(buf)))
+
+	recoginzeFace, err := rec.RecognizeSingle(buf)
 	if err != nil {
 		log.Println("Can't recognize: %v", err)
 		_ = json.NewEncoder(w).Encode(Response{Status: "Data error\n"})
@@ -102,7 +101,7 @@ func InitRecognizerDB() []string {
 	return names
 }
 
-func SaveFace(file multipart.File) (written int64, err error) {
+func SaveFace(file io.Reader) (written int64, err error) {
 	t := time.Now()
 	fileName := t.Format(time.RFC3339) + ".jpg"
 	saveFile := filepath.Join(IdentifyFacesDirectory, fileName)
@@ -110,6 +109,7 @@ func SaveFace(file multipart.File) (written int64, err error) {
 	if err != nil {
 		return 0, err
 	}
+
 	return io.Copy(f, file)
 }
 
